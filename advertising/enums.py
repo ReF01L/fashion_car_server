bdd6a141661244c0423a07ee083503d20586b8e2
ref0il <dsalushkin@gmail.com>
2023-09-13 01:35:25 +1000

    Не помню что сделал

---
+++ advertising/enums.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+
+class VerboseNameEnum(Enum):
+    pass
+
+
+class VerboseNamePluralEnum(Enum):
+    pass
---
+++ advertising/models.py
@@ -0,0 +1,5 @@
+from django.db import models
+
+
+class Advertising(models.Model):
+    pass
